0bd53a6259bcb3b273831f40186c8cc68e1bc293
Melvin <melvin0008@gmail.com>
2014-12-28 20:37:02 +0530

    added new files

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+ruby '2.1.5'
+
+gem 'sinatra',            '1.4.5'
+gem 'rest_client',		  '1.8.2'
+gem 'json',				  '1.8.1'
---
+++ app.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+require 'json'
+require 'rest_client'
+
+
+
+get '/convert' do
+	message=params['txtweb-message'].split(' ')
+	if message.length ==2
+		begin
+			countryfrom=message[0].upcase
+			countryto=message[1].upcase
+			parameter="#{countryfrom}_#{countryto}"
+			query="http://www.freecurrencyconverterapi.com/api/v2/convert?q=#{parameter}&compact=y"
+			api_result = RestClient.get query
+			dict_result=JSON.parse(api_result)
+			return_val= "1 #{countryfrom} => #{dict_result[parameter]['val']} #{countryto}"
+			erb :txtcurrency, :locals => { :returnval => return_val }		
+		rescue NameError
+			
+		end
+	else
+		
+	end
+end
