290cfc81656637c4fc7760e5c3c2a01f0a85f631
Melvin <melvin0008@gmail.com>
2014-12-29 00:44:27 +0530

    changed exception format

--- app.rb
+++ app.rb
@@ -5,22 +5,26 @@ require 'rest_client'
 
 
 get '/' do
-	begin
+	
+	if defined? params['txtweb-message']
 		message=params['txtweb-message'].split(' ')
-		if message.length ==2
-			
+		if message.length ==2	
 			countryfrom=message[0].upcase
 			countryto=message[1].upcase
 			parameter="#{countryfrom}_#{countryto}"
 			query="http://www.freecurrencyconverterapi.com/api/v2/convert?q=#{parameter}&compact=y"
-			api_result = RestClient.get query
-			dict_result=JSON.parse(api_result)
+			begin
+				api_result = RestClient.get query
+				dict_result=JSON.parse(api_result)
+			rescue Exception
+				erb :errorPage
+			end
 			return_val= "1 #{countryfrom} => #{dict_result[parameter]['val']} #{countryto}"
 			erb :txtcurrency, :locals => { :returnval => return_val }		
 		else
 			erb :errorPage
 		end
-	rescue Exception
-		erb:errorPage
+	else
+		erb :errorPage
 	end
 end
